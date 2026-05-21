3099e897662d4c1c3c001688588851d1e2072814
martin0800 <castro29070731@gmail.com>
2026-05-21 17:19:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+﻿// Función para simular el botón de ver más o comprar
+function avisarCompra(nombreProducto) {
+    alert("¡Pronto tendremos más detalles sobre el producto: " + nombreProducto + "!");
+}
